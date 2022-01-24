712ebf430c8c1416af89035bb960a2dc36de02c0
lanebx <havchikos@gmail.com>
2022-01-24 15:59:41 +0200

    with rxjs double click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {interval, take} from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import {buffer, filter, fromEvent, interval, Observable, throttleTime, merge, debounceTime, map} from 'rxjs';
+import { delay, take, exhaustMap } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -9,13 +11,32 @@ import {interval, take} from 'rxjs';
 export class AppComponent implements OnInit{
   title = 'stopwatch';
   startStopwatch?: boolean;
-  interval: any;
   // @ts-ignore
   time = new Date(null);
-  source : any;
+  source$ : any;
+  clicks = fromEvent(document, 'click');
+  lastStopedTime: any = 0;
 
   ngOnInit() {
     this.startStopwatch = false;
+
+    // @ts-ignore
+    const waitButton$ = fromEvent(document.getElementById('waitButton'), 'click')
+
+    const buff$ = waitButton$.pipe(
+      debounceTime(300),
+    )
+
+    const click$ = waitButton$.pipe(
+      buffer(buff$),
+      map(list => {
+        return list.length;
+      }),
+      filter(x => x === 2),
+    )
+    .subscribe(() => {
+      this.waitTimer();
+    })
   }
 
   startStop() {
@@ -25,9 +46,9 @@ export class AppComponent implements OnInit{
   }
 
   startTimer() {
-    this.source = interval(1000)
+    this.source$ = interval(1000)
       .subscribe((val) => {
-        this.time.setSeconds(val)
+        this.time.setSeconds((this.time.getSeconds() + 1))
       })
 
     this.startStopwatch = true;
@@ -36,27 +57,22 @@ export class AppComponent implements OnInit{
   stopTimer() {
     // @ts-ignore
     this.time = new Date(null);
-    clearInterval(this.interval);
+    this.source$.unsubscribe();
 
     this.startStopwatch = false;
   }
 
   waitTimer() {
+    this.source$.unsubscribe();
+
     this.startStopwatch = false;
-    clearInterval(this.interval);
   }
 
   resetTimer() {
-    this.time.setSeconds(0);
+    // @ts-ignore
+    this.time = new Date(null);
+    this.source$.unsubscribe();
+
+    this.startTimer()
   }
-
-  startT() {
-    interval(1000)
-  }
-
-  stopT() {
-
-  }
-
-
 }
